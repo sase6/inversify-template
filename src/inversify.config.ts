1b4f55d6fd5dc9c799dac9d2f837803020c62f22
sase <bs.saase@gmail.com>
2022-11-07 11:29:21 -0500

    Binded prmotion model to container

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -3,6 +3,7 @@ import { Container } from "inversify";
 // injections
 import Customer from "./model/Customer";
 import Pet from "./model/Pet";
+import Promotion from "./model/Promotion";
 
 import setupDb from "./db/db-setup";
 setupDb();
@@ -14,5 +15,6 @@ const container = new Container({
 
 container.bind("Customer").toConstantValue(Customer);
 container.bind("Pet").toConstantValue(Pet);
+container.bind("Promotion").toConstantValue(Promotion);
 
 export { container };
